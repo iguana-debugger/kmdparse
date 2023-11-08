6cd359895861eb72f4c80697398faab82dbc74e5
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-11-08 17:00:32 +0000

    Working hex function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use nom::{
+    branch::alt,
     bytes::complete::{tag, take_while},
-    character::complete::hex_digit1,
-    combinator::map_res,
+    character::{complete::hex_digit1, is_hex_digit},
+    combinator::{map_res, recognize},
     number::complete::hex_u32,
     AsChar, IResult, Parser,
 };
@@ -14,29 +15,35 @@ fn kmd_tag(input: &str) -> IResult<&str, &str> {
 }
 
 fn hex_to_int(input: &str) -> Result<u32, std::num::ParseIntError> {
-    u32::from_str_radix(input, 16)
+    let input_no_space = input.replace(" ", "");
+    u32::from_str_radix(&input_no_space, 16)
 }
 
 fn hex(input: &str) -> IResult<&str, u32> {
-    let (remaining, hex): (&str, &str) = take_while(|c: char| c.is_alphanum() || c == ' ')(input)?;
+    let (remaining, hex): (&str, &str) =
+        take_while(|c: char| is_hex_digit(c as u8) || c == ' ')(input)?;
 
     // // Removes the whitespace by splitting by whitespace and sticking the splits back together
-    // let hex_no_space = hex.split(' ').collect::<Vec<_>>().concat();
+    // let hex_no_space = hex.replace(" ", "");
 
     // // The hex_digit1 is a bit redundant here, but oh well
-    // let (_, res) = map_res(&hex_no_space, hex_to_int)(&hex_no_space)?;
+    // let (_, res) = map_res(hex_digit1, hex_to_int)(hex)?;
 
-    let res = hex
-        .split(' ')
-        .map(|hex_str| {
-            hex_to_int(hex_str).map_err(|_| {
-                nom::Err::Error(nom::error::Error::new(
-                    input,
-                    nom::error::ErrorKind::HexDigit,
-                ))
-            })
-        })
-        .product::<Result<u32, _>>()?; // Need to do binary shifty stuff
+    let res = hex_to_int(hex).map_err(|_| {
+        nom::Err::Error(nom::error::Error::new(
+            input,
+            nom::error::ErrorKind::HexDigit,
+        ))
+    })?;
+
+    // let res = hex.split(' ').map(|hex_str| {
+    //     hex_to_int(hex_str).map_err(|_| {
+    //         nom::Err::Error(nom::error::Error::new(
+    //             input,
+    //             nom::error::ErrorKind::HexDigit,
+    //         ))
+    //     })
+    // });
 
     Ok((remaining, res))
 }
@@ -74,21 +81,23 @@ mod tests {
     fn test_hex_valid() {
         (0..u32::MAX)
             .into_par_iter()
-            .for_each(|i| assert_done_and_eq!(hex(&format!("{:X}", i)), i))
+            .for_each(|i| assert_done_and_eq!(hex(&format!("{:X}", i)), i));
     }
 
     #[test]
     fn test_hex_invalid() {
         assert_error!(hex("notamemoryaddress"));
+        assert_error!(hex(""));
     }
 
     #[test]
     fn test_hex_overflow() {
-        assert_error!(hex("FFFFFFFFFFFFFFFF"))
+        assert_error!(hex("FFFFFFFFFFFFFFFF"));
     }
 
     #[test]
     fn test_hex_spaces() {
-        assert_done_and_eq!(hex("DE AD BE EF"), 0xDEADBEEF)
+        assert_done_and_eq!(hex("DE AD BE EF"), 0xDEADBEEF);
+        assert_done_and_eq!(hex("0A 00"), 0x0A00);
     }
 }
