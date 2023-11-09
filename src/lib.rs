79bd3d43466d25d59d7779e18380a8a718aaaa6f
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-11-09 16:42:37 +0000

    CRLF handling (note that I'm not including the whole example file)

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,11 @@ mod token;
 
 use line::Line;
 use nom::{
+    branch::alt,
     bytes::complete::{tag, take_while},
     character::{complete::char, is_hex_digit, is_space},
+    combinator::opt,
+    multi::many1,
     IResult,
 };
 use token::Token;
@@ -12,7 +15,7 @@ use token::Token;
 /// Takes the comment section of a KMD line. This parser basically just takes everything up until a
 /// newline, trimming the newline in the process. Note that \r\n will probably do weird things here.
 fn comment(input: &str) -> IResult<&str, &str> {
-    take_while(|c| c != '\n')(input)
+    take_while(|c| c != '\r' && c != '\n')(input)
 }
 
 fn hex_to_int(input: &str) -> Result<u32, std::num::ParseIntError> {
@@ -36,7 +39,7 @@ fn hex(input: &str) -> IResult<&str, u32> {
 
 /// Parses the KMD tag at the start of every .kmd file
 fn kmd_tag(input: &str) -> IResult<&str, &str> {
-    tag("KMD\n")(input)
+    alt((tag("KMD\r\n"), tag("KMD\n")))(input)
 }
 
 fn line(input: &str) -> IResult<&str, Token> {
@@ -47,10 +50,11 @@ fn line(input: &str) -> IResult<&str, Token> {
     let (remaining, memory_address) = hex(input)?;
     let (remaining, _) = char(':')(remaining)?;
     let (remaining, _) = take_while(|c| is_space(c as u8))(remaining)?;
-    let (remaining, word) = hex(remaining)?;
+    let (remaining, word) = opt(hex)(remaining)?;
     let (remaining, _) = take_while(|c| is_space(c as u8))(remaining)?;
     let (remaining, _) = char(';')(remaining)?;
     let (remaining, comment) = comment(remaining)?;
+    let (remaining, _) = alt((tag("\r\n"), tag("\n")))(remaining)?;
 
     Ok((
         remaining,
@@ -58,15 +62,17 @@ fn line(input: &str) -> IResult<&str, Token> {
     ))
 }
 
-pub fn parse_kmd(input: &str) -> IResult<Vec<Token>, &str> {
-    // let (remaining, _) = kmd_tag(input)?;
+pub fn parse_kmd(input: &str) -> IResult<&str, Vec<Token>> {
+    let (remaining, _) = kmd_tag(input)?;
 
-    todo!()
+    let (remaining, lines) = many1(line)(input)?;
+
+    Ok((remaining, lines))
 }
 
 #[cfg(test)]
 mod tests {
-    use nom_test_helpers::{assert_done_and_eq, assert_error, assert_finished};
+    use nom_test_helpers::{assert_done, assert_done_and_eq, assert_error, assert_finished};
     use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
     use super::*;
@@ -128,7 +134,7 @@ mod tests {
     fn test_line_line() {
         let expected = Line::new(
             0x00000008,
-            0x42757A7A,
+            Some(0x42757A7A),
             " buzz    DEFB \"Buzz\",0".to_string(),
         );
 
@@ -136,5 +142,21 @@ mod tests {
             line("00000008: 42 75 7A 7A ; buzz    DEFB \"Buzz\",0\n"),
             Token::Line(expected)
         );
+    }
+
+    #[test]
+    fn test_parse_kmd_valid() {
+        let example = include_str!("01-fourbuzz.kmd");
+        let (remaining, lines) = parse_kmd(example).unwrap();
+
+        assert_eq!(lines.len(), 65)
+    }
+
+    #[test]
+    fn test_parse_kmd_valid_crlf() {
+        let example = include_str!("01-fourbuzz.kmd").replace("\n", "\r\n");
+        let (remaining, lines) = parse_kmd(&example).unwrap();
+
+        assert_eq!(lines.len(), 65)
     }
 }
--- src/line.rs
+++ src/line.rs
@@ -1,12 +1,12 @@
 #[derive(Debug, PartialEq)]
 pub struct Line {
     memory_address: u32,
-    word: u32,
+    word: Option<u32>,
     comment: String,
 }
 
 impl Line {
-    pub fn new(memory_address: u32, word: u32, comment: String) -> Self {
+    pub fn new(memory_address: u32, word: Option<u32>, comment: String) -> Self {
         Self {
             memory_address,
             word,
